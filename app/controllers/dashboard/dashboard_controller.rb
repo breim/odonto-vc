9e4c7cd4a5f8ee837f4ac9be7d7553e296f0c2f4
breim <breim@protonmail.ch>
2019-03-18 04:22:10 -0300

    fix dashbord module name

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # app/controllers/admin/admin_controller.rb
-module Admin
-  # AdminController
-  class AdminController < ApplicationController
+module Dashboard
+  # DashboardController
+  class DashboardController < ApplicationController
     before_action :authenticate_user!
   end
 end
