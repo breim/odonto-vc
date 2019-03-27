4b8b0ff7b317f66e941d706ae3a6b386e00ffffb
breim <breim@protonmail.ch>
2019-03-27 20:04:03 -0300

    fix modal load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@
 //= require themes/mintone/bootstrap.js
 //= require themes/mintone/popper.min
 //= require themes/mintone/perfect-scrollbar.jquery.min
-//= require themes/mintone/sidebarmenu
 //= require themes/mintone/custom
 //= require themes/mintone/jquery.toast
 //= require themes/mintone/jQuery.style.switcher
