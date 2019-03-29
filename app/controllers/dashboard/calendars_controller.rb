19ad8cf2b3ccabb04e9bbc0dc69f715dae7f005d
breim <breim@protonmail.ch>
2019-03-29 02:00:48 -0300

    fix search for customers on right path

--- app/controllers/dashboard/calendars_controller.rb
+++ app/controllers/dashboard/calendars_controller.rb
@@ -3,10 +3,5 @@ module Dashboard
   # Controller
   class CalendarsController < Dashboard::DashboardController
     def index; end
-
-    def search
-      @customers = Customer.joins(:user).where(user_id: current_user.id, deleted: false).search(params[:search])
-                           .order('created_at desc').limit(15)
-    end
   end
 end
