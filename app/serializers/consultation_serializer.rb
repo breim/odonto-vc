5a199c8925339d57e787e6f4d2520770a38400c1
Henrique Breim <henrique@breim.com.br>
2019-03-30 16:41:47 -0300

    create consultation serializer

---
+++ app/serializers/consultation_serializer.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class ConsultationSerializer < ActiveModel::Serializer
+  attributes :id, :title, :start, :end, :token, :disabled
+
+  def title
+    object.customer_name
+  end
+
+  def start
+    "#{object.date}T#{object.hour}:00"
+  end
+
+  def end
+    sum_date_to_min(object.date, object.hour, object.duration).try(:strftime, '%Y-%m-%dT%H:%M:00')
+  end
+
+  private
+
+  def sum_date_to_min(date, hour, duration)
+    "#{date} #{hour}".to_time + hours_to_minutes(duration).minutes unless duration.nil?
+  end
+
+  def hours_to_minutes(val)
+    val.split(':')[0].to_i * 60 + val.split(':')[1].to_i unless val.nil?
+  end
+end
