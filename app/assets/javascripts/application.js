d5afb38d1b76803e3f10be710c20ae060d9d3378
breim <breim@protonmail.ch>
2019-03-31 02:11:30 -0300

    fix modal resize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,10 @@
 //= require vendors/fullCalendar
 
 //= require themes/mintone/bootstrap.js
+//= require themes/mintone/modal-fix.js
 //= require themes/mintone/popper.min
 //= require themes/mintone/perfect-scrollbar.jquery.min
 //= require themes/mintone/custom
 //= require themes/mintone/jquery.toast
 //= require themes/mintone/jQuery.style.switcher
+
---
+++ app/assets/javascripts/themes/mintone/modal-fix.js
@@ -0,0 +1,3 @@
+$(document.body).on('hide.bs.modal,hidden.bs.modal', function () {
+  $('body').css('padding-right','0');
+});
