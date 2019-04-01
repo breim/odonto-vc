22709919f82d2b16a8cd19becc4059044ecad575
breim <breim@protonmail.ch>
2019-04-01 09:36:58 -0300

    add controller to search

--- app/controllers/dashboard/calendars_controller.rb
+++ app/controllers/dashboard/calendars_controller.rb
@@ -4,6 +4,9 @@
 module Dashboard
   # Controller
   class CalendarsController < Dashboard::DashboardController
-    def index; end
+    def index
+      @customers = Customer.joins(:user).where(user_id: current_user.id, deleted: false).search(params[:search])
+                           .order('created_at desc').limit(15).decorate if params[:search]
+    end
   end
 end
