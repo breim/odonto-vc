e873b84bd20a4f3769b3cee661a20edef7ed6062
breim <breim@protonmail.ch>
2019-04-11 12:29:01 -0300

    add tickets to admin crud

---
+++ app/controllers/admin/tickets_controller.rb
@@ -0,0 +1,41 @@
+# Extend module
+module Admin
+  # Controller
+  class TicketsController < Admin::AdminController
+    before_action :set_ticket, only: %i[show edit update]
+
+    respond_to :html
+
+    def index
+      @tickets = if params[:search]
+                   Ticket.search(params[:search]).paginate(page: params[:page])
+                 else
+                   Ticket.paginate(page: params[:page]).order(created_at: :desc)
+                 end
+      respond_with(@tickets)
+    end
+
+    def show
+      respond_with(@ticket)
+    end
+
+    def edit; end
+
+    def update
+      @ticket.update(ticket_params)
+      respond_with(@ticket, location: admin_tickets_path)
+    end
+
+    def destroy; end
+
+    private
+
+    def set_ticket
+      @ticket = Ticket.find(params[:id])
+    end
+
+    def ticket_params
+      params.require(:ticket).permit(:solved)
+    end
+  end
+end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,8 +3,10 @@
 class Ticket < ApplicationRecord
   belongs_to :user, optional: true
 
+  self.per_page = 15
+
   include PgSearch
-  pg_search_scope :search, against: %i[name celphone email],
+  pg_search_scope :search, against: %i[name phone email],
                            using: {
                              tsearch: { prefix: true }
                            }
