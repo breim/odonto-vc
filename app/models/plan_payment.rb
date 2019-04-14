62568ebe37fab6400e7814e3eb427731ec393489
breim <breim@protonmail.ch>
2019-04-13 23:43:18 -0300

    fix model

--- app/models/plan_payment.rb
+++ app/models/plan_payment.rb
@@ -5,11 +5,13 @@ class PlanPayment < ApplicationRecord
 
   usar_como_dinheiro :price
 
-  def self.bill_card(card_hash)
+  def self.bill_card(card_hash_or_id)
     PagarMe.api_key = ENV['pagarme_api_key']
-    transaction = PagarMe::Transaction.new(
-      amount: ENV['plan_price'], card_hash: card_hash
-    )
+    transaction = if card_hash_or_id.length > 40
+      PagarMe::Transaction.new(amount: ENV['plan_price'], card_hash: card_hash_or_id)
+    else
+      PagarMe::Transaction.new(amount: ENV['plan_price'], card: PagarMe::Card.find_by_id(card_hash_or_id))
+    end
     transaction.charge
   end
 end
