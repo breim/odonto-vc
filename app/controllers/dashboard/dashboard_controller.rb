12af1fe1daff644af2b8dba229cd556cc78bf060
Henrique Breim <henrique@breim.com.br>
2019-04-15 14:06:47 -0300

    restric dashboard to blocked users

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -5,7 +5,17 @@ module Dashboard
   # DashboardController
   class DashboardController < ApplicationController
     before_action :authenticate_user!
+    before_action :disabled_or_blocked_user
 
     layout 'dashboard'
+
+    private
+
+    def disabled_or_blocked_user
+      return false unless current_user.disabled?
+      sign_out current_user
+      flash[:error] = 'Sua conta foi desabilitada.'
+      redirect_to root_path
+    end
   end
 end
