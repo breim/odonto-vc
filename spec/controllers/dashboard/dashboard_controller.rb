f9f77668e8d4ff4a374f8f99abd93b0728c774cd
Henrique Breim <henrique@breim.com.br>
2019-04-15 23:55:07 -0300

    create an example of disabled user in spec

--- spec/controllers/dashboard/dashboard_controller.rb
+++ spec/controllers/dashboard/dashboard_controller.rb
@@ -2,8 +2,9 @@
 
 RSpec.describe Dashboard::DashboardController, type: :controller do
   let!(:user) { create(:user) }
+  let!(:user_disabled) { create(:user, disabled: true) }
 
-  describe 'user_signed_in? helper' do
+  describe 'authenticate_user helper' do
     context 'with user logged in' do
       before do
         sign_in user
@@ -16,6 +17,20 @@ RSpec.describe Dashboard::DashboardController, type: :controller do
 
     context 'without user logged in' do
       it 'returns false' do
+        expect(controller.send(:user_signed_in?)).to be_falsey
+      end
+    end
+  end
+
+  describe 'disabled_or_blocked_user' do
+    context 'when user logged but disabled flag enabled' do
+      before do
+        sign_in user_disabled
+      end
+
+      it 'return signed out user' do
+        sign_out user_disabled
+        expect(subject.root_path).to eq('/')
         expect(controller.send(:user_signed_in?)).to be_falsey
       end
     end
