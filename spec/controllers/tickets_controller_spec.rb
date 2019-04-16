9196ff34b8a3db1d6ac072542cc842f48a03d41b
Henrique Breim <henrique@breim.com.br>
2019-04-15 22:57:43 -0300

    rubocop fix

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-  describe "POST #create" do
-    it "create remote ticket" do
+  describe 'POST #create' do
+    it 'create remote ticket' do
       post :create, params: { ticket: attributes_for(:ticket), format: :js }
-      expect(response.content_type).to eq "text/javascript"
+      expect(response.content_type).to eq 'text/javascript'
     end
   end
 end
