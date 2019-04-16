63cf9b216c27a820c45429f7a8b2fa0d7675085e
breim <breim@protonmail.ch>
2019-04-16 19:00:09 -0300

    create calendars spec

--- spec/controllers/dashboard/calendars_controller.rb
+++ spec/controllers/dashboard/calendars_controller.rb
@@ -3,21 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::CalendarsController, type: :controller do
-  let(:user) { FactoryBot.create(:user) }
+  let!(:user) { create(:user) }
+  let!(:customer) { create(:customer, user_id: user.id) }
+
+  before do
+    sign_in user
+  end
 
   describe 'GET #index' do
-    context 'with user not logged' do
-      it 'return 401 error' do
+    context 'only get' do
+      it 'return 200 status code' do
         get :index
-        expect(response.status).to eq(302)
+        expect(response.status).to eq(200)
       end
     end
 
-    context 'with user loged in' do
+    context 'get with search params to find customers' do
       it 'return 200 status code' do
-        sign_in user
-        get :index
-        expect(response.status).to eq(200)
+        @customers = Customer.joins(:user).where(user_id: user.id, deleted: false).search(customer.name)
+        expect(@customers).to eq([customer])
       end
     end
   end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'faker'
+
+FactoryBot.define do
+  factory :customer do
+    name { Faker::Name.name }
+    birthday { Date.today - 20.years }
+    celphone { Faker::PhoneNumber.cell_phone }
+    phone { Faker::PhoneNumber.cell_phone }
+    user_id { SecureRandom.hex(16) }
+    gender { 'M' }
+    email { Faker::Internet.email }
+    code { '21123312' }
+    zipcode { Faker::Address.zip }
+    address { Faker::Address.street_address }
+    distric { Faker::Address.community }
+    state { Faker::Address.state }
+    city { Faker::Address.city }
+    complement { Faker::Address.secondary_address }
+    cpf { Faker::IDNumber.brazilian_citizen_number }
+    user
+  end
+end
