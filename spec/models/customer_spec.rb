abd09668a43333360ed7cf67f312f16a05af9cb5
Henrique Breim <henrique@breim.com.br>
2019-04-18 23:30:43 -0300

    test for cpf format

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe Customer, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'self methods' do
+    it 'save only digits when save CPF' do
+      customer = build(:customer, cpf: '999.999.999-99')
+      expect(customer.build_cpf).to eq('99999999999')
+    end
+  end
 end
