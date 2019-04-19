1d57b9cb4627edd4422b38746a41693b79de2857
Henrique Breim <henrique@breim.com.br>
2019-04-18 23:11:06 -0300

    fix dumb tests

--- spec/controllers/dashboard/calendars_controller.rb
+++ spec/controllers/dashboard/calendars_controller.rb
@@ -3,26 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::CalendarsController, type: :controller do
-  let!(:user) { create(:user) }
-  let!(:customer) { create(:customer, user_id: user.id) }
+  let(:user) { create(:user) }
+  let(:customer) { create(:customer, user_id: user.id) }
 
   before do
     sign_in user
   end
 
   describe 'GET #index' do
-    context 'only get' do
-      it 'return 200 status code' do
-        get :index
-        expect(response.status).to eq(200)
-      end
+    it 'return 200 params' do
+      get :index
+      expect(response.status).to eq(200)
     end
 
-    context 'get with search params to find customers' do
-      it 'return 200 status code' do
-        @customers = Customer.joins(:user).where(user_id: user.id, deleted: false).search(customer.name)
-        expect(@customers).to eq([customer])
-      end
+    it 'return 200 status code' do
+      get :index, params: { search: customer.name }, xhr: true
+      expect(assigns(:customers)).to include(customer)
     end
   end
 end
