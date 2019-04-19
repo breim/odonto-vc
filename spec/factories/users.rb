5beb9f169dae70a07338ea689e21e4dc7eeff6e0
Henrique Breim <henrique@breim.com.br>
2019-04-18 23:19:50 -0300

    fix some specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,16 +10,4 @@ FactoryBot.define do
     token { SecureRandom.hex(16) }
     disabled { false }
   end
-
-  factory :facebook_provider do
-    provider { 'facebook' }
-    uid { '102020' }
-    info {
-      email { Faker::Internet.free_email }
-      name { Faker::Name.name }
-      first_name { Faker::Name.name }
-      last_name { Faker::Name.name }
-      image { Faker::Avatar.image }
-    }
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
-  let(:facebook_provider) { build_stubbed :facebook_provider }
 
   describe 'associations' do
     it { is_expected.to have_many :customers }
@@ -15,16 +14,12 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    
+  end
+
+  describe 'self methods' do
     it 'when create user generate oauth token' do
       user = build(:user)
       expect(user.build_token).to eq user.token
-    end
-
-    it 'when user login with facebook #from_omniauth' do
-     # a = build_stubbed(:facebook_provider)
-     # puts a.inspect
-     # puts "============="
     end
   end
 end
