46b6efb9b575c1a3da141957001c923739118657
Henrique Breim <henrique@breim.com.br>
2019-04-20 00:01:20 -0300

    test navbar_header_link

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,31 +5,21 @@ require 'spec_helper'
 describe ApplicationHelper do
 
   describe '#navbar_header_link' do
-
-    before do
-      allow(helper).to receive(:controller_name).and_return('admin')
-    end
-
     it 'when current controller is admin' do
-      expect(helper.navbar_header_link('admin')).to eq '/admin'
+      allow(helper).to receive(:controller_name).and_return('admin')
+      expect(helper.navbar_header_link(helper.controller_name)).to eq '/admin'
     end
 
-    #controller_name.split('/').first == 'admin' ? '/admin' : '/dashboard'
-
+    it 'when current controller is not admin' do
+      allow(helper).to receive(:controller_name).and_return('dashboard')
+      expect(helper.navbar_header_link(helper.controller_name)).to eq '/dashboard'
+    end
   end
 
-
   describe '#current_link_helper' do
-    before do
-      allow(helper).to receive(:controller_name).and_return('calendars')
-    end
-
     it 'when current controller is active' do
+      allow(helper).to receive(:controller_name).and_return('calendars')
       expect(helper.current_link_helper(helper.controller_name)).to eq 'active'
     end
   end
-
-
-
-
 end
