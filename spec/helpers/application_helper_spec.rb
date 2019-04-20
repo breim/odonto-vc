7b274ffd04442c11108b9189e2c4fbd89a609791
Henrique Breim <henrique@breim.com.br>
2019-04-19 23:59:06 -0300

    fix dumb test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,14 +3,33 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe '#current_link_helper' do
 
+  describe '#navbar_header_link' do
+
+    before do
+      allow(helper).to receive(:controller_name).and_return('admin')
+    end
+
+    it 'when current controller is admin' do
+      expect(helper.navbar_header_link('admin')).to eq '/admin'
+    end
+
+    #controller_name.split('/').first == 'admin' ? '/admin' : '/dashboard'
+
+  end
+
+
+  describe '#current_link_helper' do
     before do
       allow(helper).to receive(:controller_name).and_return('calendars')
     end
 
     it 'when current controller is active' do
-      expect(helper.current_link_helper('calendars')).to eq 'active'
+      expect(helper.current_link_helper(helper.controller_name)).to eq 'active'
     end
   end
+
+
+
+
 end
