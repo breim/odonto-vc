4a7947e8f08b8497d68b9f93e06d1857ce67ebd3
Henrique Breim <henrique@breim.com.br>
2019-04-20 00:23:57 -0300

    create customers index test

---
+++ spec/controllers/dashboard/customers_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Dashboard::CustomersController, type: :controller do
+  let(:user) { create(:user) }
+  let(:customers) { create_list(:customer, 5, user_id: user.id) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET #index' do
+    it 'return some customers' do
+      get :index
+      expect(assigns(:customers)).to match_array(customers)
+    end
+
+    it 'search customer name' do
+      customer = customers.first
+      get :index, params: { search: customer.name }, xhr: true
+      expect(assigns(:customers)).to include(customer)
+    end
+  end
+end
