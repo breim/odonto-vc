ed64b864648a9375adfbf05c2dfd6bbeac780f25
Henrique Breim <henrique@breim.com.br>
2019-04-26 04:10:57 -0300

    clean test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,14 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  let(:user) { create(:user) }
-
-  before do
-    # sign_in user
-  end
-
-  it 'redirect user after login #after_sign_in_path_for' do
-    # expect(response).to redirect_to(dashboard_controllers_path)
-    # expect(controller.send(:user_signed_in?)).to be_truthy
-  end
+  # let(:user) { create(:user) }
 end
