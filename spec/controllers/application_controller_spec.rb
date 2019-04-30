4bd61c50b1612662ffb2ff5065b915532d514974
breim <breim@protonmail.ch>
2019-04-30 02:05:14 -0300

    test method in application controller

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  # let(:user) { create(:user) }
+  before do
+    @user = create(:user)
+  end
+
+  describe '#after_sign_in_path_for' do
+    it 'redirects to the /dashboard/calendars page' do
+      expect(controller.send(:after_sign_in_path_for, @user)).to eq(dashboard_calendars_path)
+    end
+  end
 end
