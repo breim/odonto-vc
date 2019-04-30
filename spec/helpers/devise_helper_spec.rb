b65da41b34f59d3442af627226056777319e7be6
breim <breim@protonmail.ch>
2019-04-30 02:20:15 -0300

    test devise messages

---
+++ spec/helpers/devise_helper_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe DeviseHelper, type: :helper do
+  let(:resource_name) { :user }
+  let(:devise_mapping) { Devise.mappings[:user] }
+
+  context 'no error messages' do
+    let(:resource) { User.new }
+    it '#devise_error_messages!' do
+      expect(devise_error_messages!).to eq('')
+    end
+  end
+
+  context 'with error messages' do
+    let(:resource) { User.create }
+    it '#devise_error_messages!' do
+      expect(devise_error_messages!).to_not eq('')
+    end
+  end
+end
