dfbec4f39b6d8357c8e1fc2b9a2904fc24a0aed0
Henrique Breim <henrique@breim.com.br>
2019-05-02 21:13:34 -0300

    remove required lib at top level

--- spec/factories/plan_payment.rb
+++ spec/factories/plan_payment.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'faker'
-
 FactoryBot.define do
   factory :plan_payment do
     price { 6990 }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'faker'
-
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
